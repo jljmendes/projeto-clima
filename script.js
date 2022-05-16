f129e56b6cf48bfce182c509a2ad531d08712a14
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-05-15 21:22:35 -0300

    Inicializando o arquivo js

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelector('.busca').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    let input = document.querySelector('#searchInput').value;
+    
+    if(input !== '') {
+        clearInfo();
+        showWarning('Carregando...');
+
+        let results = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=
+        ${encodeURI(input)}&units=metric&lang=pt_br&appid=d06cdb298fafc83c520d5ab677fc477e`);
+        let json = await results.json();
+
+        if(json.cod === 200) {
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            });
+        } else {
+            clearInfo();
+            showWarning('Não encontramos esta localização.');
+        }
+    } else {
+        clearInfo();
+    }
+});
