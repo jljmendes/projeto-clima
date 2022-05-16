26afffdadad543962b90c278a0ceadc668f9a41f
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-05-16 20:45:40 -0300

    Criando a função showinfo

--- script.js
+++ script.js
@@ -28,3 +28,19 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
         clearInfo();
     }
 });
+
+function showInfo(obj) {
+    showWarning('');
+
+    document.querySelector('.titulo').innerHTML = `${obj.name}, ${obj.country}`;
+    
+    document.querySelector('.tempInfo').innerHTML = `${obj.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${obj.windSpeed} <span>km/h</span>`;
+
+    document.querySelector('.temp img').setAttribute('src', 
+    `http://openweathermap.org/img/wn/${obj.tempIcon}@2x.png`);
+    
+    document.querySelector('.ventoPonto').style.transform = `rotate(${obj.windAngle-90}deg)`;
+
+    document.querySelector('.resultado').style.display = 'block';
+}
